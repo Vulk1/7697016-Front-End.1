73c0b7cc40e1644cdef72167edf4284259813b8b
yaakoub <yaakoub.leguennec@gmail.com>
2026-01-06 19:54:52 +0100

    Ajout de la fonctionnalité affichage de tous les produits

--- pieces.js
+++ pieces.js
@@ -1,28 +1,36 @@
 const response = await fetch("pieces-autos.json");
 const pieces = await response.json();
 
-const article = pieces[0];
-
-const imageElement = document.createElement("img");
-imageElement.src = article.image;
-const nomElement = document.createElement("h2");
-nomElement.textContent = article.nom;
-const prixElement = document.createElement("p");
-prixElement.textContent = `Prix : ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
-const categorieElement = document.createElement("p");
-categorieElement.textContent = article.categorie ?? "(aucune catégorie)";
-const descriptionElement = document.createElement("p");
-descriptionElement.textContent = article.description ?? "Pas de description pour le moment.";
-const stockElement = document.createElement("p");
-stockElement.textContent = article.disponibilite ? "En stock" : "Rupture de stock";
+let nbArticle = pieces.length;
 
 //Rattachement de nos balises au DOM
 const sectionFiches = document.querySelector(".fiches");
 
-sectionFiches.appendChild(imageElement);
-sectionFiches.appendChild(nomElement);
-sectionFiches.appendChild(prixElement);
-sectionFiches.appendChild(categorieElement);
-sectionFiches.appendChild(descriptionElement);
-sectionFiches.appendChild(stockElement);
+for(let i=0; i < nbArticle; i++) {
+    const article = pieces[i];
 
+    const pieceElement = document.createElement("article");
+
+    const imageElement = document.createElement("img");
+    imageElement.src = article.image;
+    const nomElement = document.createElement("h2");
+    nomElement.textContent = article.nom;
+    const prixElement = document.createElement("p");
+    prixElement.textContent = `Prix : ${article.prix} € (${article.prix < 35 ? "€" : "€€€"})`;
+    const categorieElement = document.createElement("p");
+    categorieElement.textContent = article.categorie ?? "(aucune catégorie)";
+    const descriptionElement = document.createElement("p");
+    descriptionElement.textContent = article.description ?? "Pas de description pour le moment.";
+    const stockElement = document.createElement("p");
+    stockElement.textContent = article.disponibilite ? "En stock" : "Rupture de stock";
+
+    
+    sectionFiches.appendChild(pieceElement);
+
+    pieceElement.appendChild(imageElement);
+    pieceElement.appendChild(nomElement);
+    pieceElement.appendChild(prixElement);
+    pieceElement.appendChild(categorieElement);
+    pieceElement.appendChild(descriptionElement);
+    pieceElement.appendChild(stockElement);
+}
